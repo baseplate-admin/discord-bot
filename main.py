c1937fd63c5f6a8c661e5d6b4b23396814322504
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2020-10-07 22:41:58 +0600

    Full Player Rewrite

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import random
 import asyncio
 from discord.ext import tasks
 from itertools import cycle
-import json
 import urllib.parse
 import urllib.request
 import re
@@ -13,52 +12,6 @@ import glob
 import os
 from discord.utils import get
 import json
-# Youtube Play Music Important stuffs which i dont know
-
-youtube_dl.utils.bug_reports_message = lambda: ''
-
-ytdl_format_options = {
-    'format': 'bestaudio/best',
-    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
-    'restrictfilenames': True,
-    'noplaylist': True,
-    'nocheckcertificate': True,
-    'ignoreerrors': False,
-    'logtostderr': False,
-    'quiet': True,
-    'no_warnings': True,
-    'default_search': 'auto',
-    'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
-}
-
-ffmpeg_options = {
-    'options': '-vn'
-}
-
-ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
-
-
-class YTDLSource(discord.PCMVolumeTransformer):
-    def __init__(self, source, *, data, volume=0.5):
-        super().__init__(source, volume)
-
-        self.data = data
-
-        self.title = data.get('title')
-        self.url = data.get('url')
-
-    @classmethod
-    async def from_url(cls, url, *, loop=None, stream=False):
-        loop = loop or asyncio.get_event_loop()
-        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
-
-        if 'entries' in data:
-            # take first item from a playlist
-            data = data['entries'][0]
-
-        filename = data['url'] if stream else ytdl.prepare_filename(data)
-        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
-
 # Get prefix
 
 def get_prefix(client, message):
@@ -227,7 +180,7 @@ async def change_status():
 # Search Function
 @client.command(aliases=[])
 async def play(ctx, *, search):
-
+    # Checks and connects to user voice channel
     global voice_check
     channel_check = ctx.message.author.voice.channel
     voice_check = get(client.voice_clients, guild=ctx.guild)
@@ -245,17 +198,42 @@ async def play(ctx, *, search):
     search_results = re.findall(r'/watch\?v=(.{11})', htm_content.read().decode())
     search_result=('https://www.youtube.com/watch?v=' + search_results[0])
 
-    # Some Constants
-    server = ctx.message.guild
-    voice_channel = server.voice_client
+    #Player Function
 
-    # End constant
-    async with ctx.typing():
+    # Remove Song MP3
+    song_there = os.path.isfile("song.mp3")
+    try:
+        if song_there:
+            os.remove("song.mp3")
+    except PermissionError:
+        print("Trying to delete song file but its being played.")
+        await ctx.send("ERROR:Music Playing")
+        return
+    await ctx.send("Getting From Youtube.....")
+    voice = get(client.voice_clients, guild=ctx.guild)
+    ydl_options = {
+        "format": "bestaudio/best",
+        "postprocessors": [{
+            "key": "FFmpegExtractAudio",
+            "preferredcodec": "mp3",
+            "preferredquality": "320",
+            }]
+    }
+    with youtube_dl.YoutubeDL(ydl_options) as ydl:
+        print("Downloading Audio Now\n")
+        ydl.download([search_result])
+    for file in os.listdir("./"):
+        if file.endswith(".mp3"):
+            name = file
+            print(f"Renamed file: {file}\n")
+            os.rename(file, "song.mp3")
+    voice.play(discord.FFmpegPCMAudio("song.mp3"), after=lambda e: print(f"{name} has finished playing"))
+    voice.source = discord.PCMVolumeTransformer(voice.source)
+    voice.source.volume = 0.07
 
-        player = await YTDLSource.from_url(search_result, loop=client.loop)
-        voice_channel.play(player, after=lambda e: print('Player error: %s' %e) if e else None)
-    await ctx.send(f'Now Playing:{player.title}')
-
+    newname = name.rsplit("-", 2)
+    await ctx.send(f"Playing, {newname}")
+    print("Playing song\n")
 #Pause Function
 @client.command()
 async def pause(ctx):
@@ -277,38 +255,15 @@ async def resume(ctx):
     else:
         await ctx.send("Music is not resumed")
 
-#Leave
-
-@client.command(name='leave', help='This command stops makes the bot leave the voice channel')
+# Stop Function
+@client.command()
 async def stop(ctx):
-    voice_client = ctx.message.guild.voice_client
-    await voice_client.disconnect()
-
-# Python to destroy webp after every 5 min
-@client.command()
-async def clear_temp(ctx):
-    z = 0
-    x = []
-    for file in os.listdir():
-        if file.endswith('.webm'):
-            x.append(file)
-            z += 1
-    await ctx.send("the total number of files: " + str(z))
-    await ctx.send("Deleting these files.")
-    await ctx.send(x)
-    for i in glob.glob("*.webm"):
-        os.remove(i)
-
-# Count WebM Files
-@client.command()
-async def count(ctx):
-    i = 0
-    x = []
-    for file in os.listdir():
-        if file.endswith('.webm'):
-            i += 1
-    await ctx.send('the total number of files: ' + str(i))
+     voice_client = ctx.message.guild.voice_client
+     await voice_client.disconnect()
 
 
 
-client.run("NzUwMzY4OTAxNDYzODAxOTg3.X05hfw.3AnVy7Wsf10qamzXjoahJIKXPYE")
+
+
+
+client.run("")
