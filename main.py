8dfbaf022792a4291b43746aaaef8c392c4ba6d5
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2020-11-27 20:56:47 +0600

    ..

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from src.main import main_function_discord
 
-TOKEN = "NzUwMzY4OTAxNDYzODAxOTg3.X05hfw.G43ky8YiEfoWGHyOUjchwHjuMwo"
+TOKEN = "NzUwMzY4OTAxNDYzODAxOTg3.X05hfw.K0-ah9qx4ZRSvlW0fFtIGfkdnKM"
 if __name__ == '__main__':
     main_function_discord(TOKEN)
