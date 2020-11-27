7ac197870cd05a54644d327845f0051bddb977c8
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2020-11-27 22:27:09 +0600

    .

--- main.py
+++
@@ -1,5 +0,0 @@
-from src.loopqueue import main_function_discord
-
-TOKEN = "NzUwMzY4OTAxNDYzODAxOTg3.X05hfw.OkZzrk51jJNmsP5nDbg_mdaGy032"
-if __name__ == '__main__':
-    main_function_discord(TOKEN)
